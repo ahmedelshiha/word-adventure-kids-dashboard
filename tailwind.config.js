d1450271411810b1d5d6a2134ad89aa42a5df2d1
Ahmed Elshiha <110566728+ahmedelshiha@users.noreply.github.com>
2025-08-04 22:40:59 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 // tailwind.config.js
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        border: 'var(--color-border)',
+      },
+      outlineColor: {
+        ring: 'var(--color-ring)',
+      },
+    },
   },
   plugins: [],
 };
