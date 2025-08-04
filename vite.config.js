001fd1e4c1954682cdb5668e1f0863c9c1b3b47c
Ahmed Elshiha <110566728+ahmedelshiha@users.noreply.github.com>
2025-08-04 21:41:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Only add aliases if you're changing the path or using custom structure
-      // Example (if needed): '@/components': '/src/components'
-    }
-  }
+  build: {
+    rollupOptions: {
+      external: ['tailwindcss'],
+    },
+  },
 });
